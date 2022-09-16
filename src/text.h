9b5f9baca0024593c80c1d8b762349d095c07014
Nicu Borta <nicuborta123@tutanota.com>
2022-09-16 12:08:34 +0300

    Update and rename text_rendering.h to text.h

--- src/text.h
+++ src/text.h
@@ -10,7 +10,7 @@ struct ft_character {
 	short advance;
 };
 
-struct text_rendering {
+struct text {
 	unsigned int VAO;
 	unsigned int VBO;
 	unsigned int shader_program;
@@ -19,15 +19,15 @@ struct text_rendering {
 	struct ft_character characters[128];
 };
 
-void text_rendering_init(const char *vertex_shader_source,
+void text_init(const char *vertex_shader_source,
 		const char *fragment_shader_source,
 		const char *font, struct text_rendering *dest);
 
-void text_rendering_perspective(vec2 size, struct text_rendering *text);
+void text_perspective(vec2 size, struct text_rendering *text);
 
-void text_rendering_render(const char *string, vec2 pos, float scale,
+void text_render(const char *string, vec2 pos, float scale,
 		vec3 color, struct text_rendering *text);
 
-void text_rendering_cleanup(struct text_rendering *text);
+void text_cleanup(struct text_rendering *text);
 
 #endif
